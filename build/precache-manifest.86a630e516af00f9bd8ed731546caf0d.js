2838ecbbbdc332ecbb25cdb3bbe60ca2e3eb9cb7
Sergii Sergiienko <sergusha.s@gmail.com>
2021-03-06 15:45:23 +0200

    npm run build

---
+++ build/precache-manifest.86a630e516af00f9bd8ed731546caf0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020c61c6c5204b6c06b5249b8139f2de",
+    "url": "/StNTF/index.html"
+  },
+  {
+    "revision": "384d050446c20a23e26c",
+    "url": "/StNTF/static/css/main.8d40b8df.chunk.css"
+  },
+  {
+    "revision": "c40afa26ac5468dd8c44",
+    "url": "/StNTF/static/js/2.38c2e76d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/StNTF/static/js/2.38c2e76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384d050446c20a23e26c",
+    "url": "/StNTF/static/js/main.7680cb24.chunk.js"
+  },
+  {
+    "revision": "c7550e20526ef38d57f7",
+    "url": "/StNTF/static/js/runtime-main.36b2f659.js"
+  },
+  {
+    "revision": "1cd62fa8c1ead9a58ff0d7ff6e993125",
+    "url": "/StNTF/static/media/img-1.1cd62fa8.jpg"
+  },
+  {
+    "revision": "a6429dccb1def14dadfc58eb62adb271",
+    "url": "/StNTF/static/media/img-2.a6429dcc.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/StNTF/static/media/img-3.66001cf8.jpg"
+  },
+  {
+    "revision": "3ea99f6017e2d689b7d18d8297a6cf1b",
+    "url": "/StNTF/static/media/img-4.3ea99f60.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/StNTF/static/media/img-5.65b53910.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/StNTF/static/media/img-6.bd11375f.jpg"
+  }
+]);
